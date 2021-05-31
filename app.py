ccb897145bf40103f3352e7f01370b86fcf1c4e2
Sayed Us Sadat <26658415+sayedsadat98@users.noreply.github.com>
2021-05-31 13:18:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from img_classification import teachable_machine_classification
 import streamlit as st
-st.title("Image Classification with Google's Teachable Machine")
-st.header("Brain Tumor MRI Classification Example")
+st.title("Welcome To Image Classification")
+st.header("Brain Tumor MRI Classification App")
 st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 
 
